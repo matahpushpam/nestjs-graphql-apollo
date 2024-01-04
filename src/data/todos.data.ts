a054d02be3e5938cf7b532e92e63fd62fc8ccd09
Pushpam <matahpushpam@gmail.com>
2024-01-04 21:42:35 +0530

    Postgres with typeorm added over graphql layer

--- src/data/todos.data.ts
+++
@@ -1,5 +0,0 @@
-export const TODOS = [
-    { id: 1, title: 'Todo #1', completed: false },
-    { id: 2, title: 'Todo #2', completed: false },
-    { id: 3, title: 'Todo #3', completed: true },
-  ];
